ab717b423e62c45f86c95082b5e6ec1f1b00e7fa
Rob Richardson <robrich@robrich.org>
2019-11-11 15:52:44 -0700

    Combine sql setup into single file

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ How to Use
 ----------
 
 1. Install .NET Core 3.0 or newer
-2. Clone or download this repository
-3. Use the files in the `sql` directory to create a database in SQL Server
-4. Set the connection string in `app/appsettings.json`
-5. Run `InsecureWebsite.sln` from Visual Studio or `dotnet run` from the command line
+2. Install SQL Server (developer edition and sql express are free)
+3. Clone or download this repository
+4. Use the files in the `sql` directory to create a database in SQL Server
+5. Set the connection string in `app/appsettings.json`
+6. Run `InsecureWebsite.sln` from Visual Studio or `dotnet run` from the command line
 
 Legal
 -----
--- sql/setup-db.sql
+++ sql/setup-db.sql
@@ -1,3 +1,24 @@
+-- Step 1: Create the database in SQL Server.
+-- Assumes the database doesn't already exist
+CREATE DATABASE InsecureWebsite
+GO
+-- Step 2: Use this database for the rest of the queries we'll do
+USE InsecureWebsite
+GO
+-- Step 3: Create the TABLE
+CREATE TABLE [dbo].[User]
+(
+[UserId] [int] NOT NULL IDENTITY(1, 1),
+[Username] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Password] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Salt] [nvarchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[IsAdmin] [bit] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[User] ADD CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED  ([UserId]) ON [PRIMARY]
+GO
+-- Step 4: Add data to the table
+-- Assumes the table is empty
 SET IDENTITY_INSERT [dbo].[User] ON
 INSERT INTO [dbo].[User] ([UserId], [Username], [Password], [Salt], [IsAdmin]) VALUES (1, N'Adam', N'secret', REPLACE(CAST( NEWID() AS nvarchar(100)), '-', ''), 0)
 INSERT INTO [dbo].[User] ([UserId], [Username], [Password], [Salt], [IsAdmin]) VALUES (2, N'Bob', N'secret', REPLACE(CAST( NEWID() AS nvarchar(100)), '-', ''), 0)
@@ -5,3 +26,11 @@ INSERT INTO [dbo].[User] ([UserId], [Username], [Password], [Salt], [IsAdmin]) V
 INSERT INTO [dbo].[User] ([UserId], [Username], [Password], [Salt], [IsAdmin]) VALUES (4, N'Rob', N'password', REPLACE(CAST( NEWID() AS nvarchar(100)), '-', ''), 0)
 INSERT INTO [dbo].[User] ([UserId], [Username], [Password], [Salt], [IsAdmin]) VALUES (5, N'David', N'secret', REPLACE(CAST( NEWID() AS nvarchar(100)), '-', ''), 0)
 SET IDENTITY_INSERT [dbo].[User] OFF
+
+
+-- When you're done, uncomment these commands to destroy the database you created:
+/*
+USE Master
+GO
+DROP DATABASE InsecureWebsite
+*/
